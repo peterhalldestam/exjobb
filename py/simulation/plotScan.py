7232b7bf3b46696940ccbbbabfc57e97f9c1a708
Peter Halldestam <peterhalldestam@outlook.com>
2022-03-21 16:02:36 +0100

    added SHOW_BORDER, if True a red curve is drawn where tCQ is inf

--- py/simulation/plotScan.py
+++ py/simulation/plotScan.py
@@ -7,9 +7,9 @@ import matplotlib.pyplot as plt
 import alphashape
 
 
-
+SHOW_BORDER = False
 SHOW_POINTS = False
-LOG_PATH = 'scan3-2.log'
+LOG_PATH = 'scan2.log'
 
 
 def main():
@@ -35,33 +35,29 @@ def main():
 
     fig, ax = plt.subplots()
 
-    points = [(n1, n2) for n1, n2 in zip(nD_, nNe_)]
-    alpha = 0.95 * alphashape.optimizealpha(points)
-    hull = alphashape.alphashape(points, alpha)
-    border = hull.exterior.coords.xy
-
-
-
     # Current plot
     ax.tricontour(nD, nNe, I_re, levels=14, linewidths=0.5, colors='k')
     cntr2 = ax.tricontourf(nD, nNe, I_re, levels=14, cmap="RdBu_r")
     fig.colorbar(cntr2, ax=ax)
 
-
     # Current quench time plot
     ax.tricontour(nD_, nNe_, tCQ_, levels=[50e-3, 150e-3], linewidths=2, linestyles=['dashed', 'dotted'])
-    # cntr2 = axs[1].tricontourf(nD_, nNe_, tCQ_, levels=14, cmap="RdBu_r")
-    # fig.colorbar(cntr2, ax=axs[1])
 
 
-    nD_inf, nNe_inf = [], []
-    for n1, n2 in zip(border[0], border[1]):
-        print(n1, n2)
-        if n1 < 1.5e22 and n2 < 8e18:
-            nD_inf.append(n1)
-            nNe_inf.append(n2)
+    if SHOW_BORDER:
+        points = [(n1, n2) for n1, n2 in zip(nD_, nNe_)]
+        alpha = 0.95 * alphashape.optimizealpha(points)
+        hull = alphashape.alphashape(points, alpha)
+        border = hull.exterior.coords.xy
 
-    ax.plot(nD_inf[:-1], nNe_inf[:-1], 'r')
+        nD_inf, nNe_inf = [], []
+        for n1, n2 in zip(border[0], border[1]):
+            print(n1, n2)
+            if n1 < 1.5e22 and n2 < 8e18:
+                nD_inf.append(n1)
+                nNe_inf.append(n2)
+
+        ax.plot(nD_inf[:-1], nNe_inf[:-1], 'r')
 
     if SHOW_POINTS:
         for t, n1, n2 in zip(tCQ, nD, nNe):
