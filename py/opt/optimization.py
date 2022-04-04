5ed82a0f3d985a4b19ba10c9f6d784c59a9835fc
Peter <peterhalldestam@outlook.com>
2022-04-04 15:00:08 +0200

    expection handling

--- py/opt/optimization.py
+++ py/opt/optimization.py
@@ -1,12 +1,9 @@
 import sys, os
 import numpy as np
 from dataclasses import dataclass
-# print(help('modules'))
-# from ..simulation import DREAMSimulation
-# from . import simulation
 
-sys.path.append(os.path.abspath('..'))
-from simulation.simulation import Simulation
+from sim.simulation import Simulation
+from opt.objective import Objective
 
 class Optimization:
 
@@ -18,21 +15,29 @@ class Optimization:
         pass
 
 
-    def __init__(self, simulation=None, verbose=True, **settings):
+    def __init__(self, sim=None, obj=None, verbose=True, **settings):
         """
         Constructor.
         """
-        if objFun is None:
-            raise AttributeError('Expected an objective function to be provided.')
+        if sim is None:
+            raise AttributeError('Expected an Simulation subclass to be provided.')
+        elif not issubclass(sim, Simulation):
+            raise TypeError(f'Expected sim to be a Simulation subclass, not {type(sim)}.')
 
-        self.simulation = simulation
+        if obj is None:
+            raise AttributeError('Expected an objective function to be provided.')
+        elif not issubclass(obj, Objective):
+            raise TypeError(f'Expected obj to be a Objective subclass, not {type(obj)}.')
+
+        self.sim = sim
+        self.obj = obj
         self.verbose = verbose
 
         # Configure settings provided by the user.
         try:
             self.settings = self.Settings(**settings)
         except TypeError as err:
-            print(f'Provided settings must exist in {Input().__dataclass_fields__.keys()}')
+            print(f'Provided settings must exist in {self.Settings().__dataclass_fields__.keys()}')
             raise err
 
 
