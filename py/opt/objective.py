6ca50202aecd4701582ed60899f14787004090da
Peter <peterhalldestam@outlook.com>
2022-04-09 15:47:29 +0200

    minor fix

--- py/opt/objective.py
+++ py/opt/objective.py
@@ -6,11 +6,17 @@ from sim.DREAM.transport import TransportSimulation
 # objective function parameters
 CRITICAL_RE_CURRENT             = 150e3
 CRITICAL_OHMIC_CURRENT          = 200e3
+
+HEAT_LOSS_WEIGHT                = 100
+TRANSPORTED_FRACTION_MAX        = 1e-2
+
+CQ_WEIGHT                       = 100
 CQ_TIME_MIN                     = 50e-3
 CQ_TIME_MAX                     = 150e-3
+
 SLOPE_LEFT                      = 3e2
 SLOPE_RIGHT                     = 3e2
-CRITICAL_TRANSPORTED_FRACTION   = 1e-1
+
 
 def sigmoid(x, x0=0, k=1):
     """
@@ -22,37 +28,53 @@ def sigmoid(x, x0=0, k=1):
     """
     return 1/2 + 1/2*np.tanh(k*(x-x0))
 
-def baseObjective(output, weight=100):
+def baseObjective(output, **kwargs):
     """
     Returns the base objective function for when optimizing disruption
     simulations using DREAM.
 
-    :param output:  DREAMSimulation output object.
-    :param weight:  Penalty for when tCQ is outside its interval.
-    """
-    # if not issubclass(output, DREAMSimulation.Output):
-    #     raise TypeError("output need to be a subclass of DREAMSimulation.Output")
+    :param output:      DREAMSimulation output object.
 
-    obj1 = output.maxRECurrent / CRITICAL_RE_CURRENT
-    obj1 = output.finalOhmicCurrent / CRITICAL_OHMIC_CURRENT
+    Optional keyword arguments:
+    :param critRE:      Critical RE current, ~1 penalty when I_re ~ critRE.
+    :param critOhm:     Critical Ohmic current, ~1 penalty when I_ohm ~ critOhm.
+    :param wCQ:         Penalty for when tCQ is outside its interval.
+    """
+    if not isinstance(output, DREAMSimulation.Output):
+        raise TypeError("output need to be a instance of DREAMSimulation.Output")
+
+    critRE  = kwargs.get('critRE',  CRITICAL_RE_CURRENT)
+    critOhm = kwargs.get('critOhm', CRITICAL_OHMIC_CURRENT)
+    wCQ     = kwargs.get('wCQ',     CQ_WEIGHT)
+
+    obj1 = output.maxRECurrent / critRE
+    obj2 = output.finalOhmicCurrent / critOhm
     obj3 = sigmoid(-output.currentQuenchTime, -CQ_TIME_MIN, SLOPE_LEFT)
     obj4 = sigmoid(output.currentQuenchTime, CQ_TIME_MAX, SLOPE_RIGHT)
-    return obj1 + weight * (obj2 + obj3)
 
-def heatLossObjective(output, weight1=100, weight2=20):
+    return obj1 + obj2 + wCQ * (obj3 + obj4)
+
+def heatLossObjective(output, **kwargs):
     """
     Returns a objective function for when optimizing disruption
     simulations using DREAM that takes into account the conducted heat losses.
 
     :param output:      DREAMSimulation output object.
-    :param weight1:     Penalty for when tCQ is outside its interval.
-    :param weight2:     Penalty for when transported fraction is too large.
-    """
-    # if not isinstance(output, TransportSimulation.Output):
-    #     raise TypeError("output need to be an instance of TransportSimulation.Output")
 
-    obj1 = self.baseObjective(output, weight=weight1)
-    obj2 = output.transportedFraction / CRITICAL_TRANSPORTED_FRACTION
+    Optional keyword arguments:
+    :param critTransp:  Critical transport fraction, ~1 penalty when it is ~ critTransp.
+    :param critRE:      Critical RE current, ~1 penalty when I_re ~ critRE.
+    :param critOhm:     Critical Ohmic current, ~1 penalty when I_ohm ~ critOhm.
+    :param wCQ:         Penalty for when tCQ is outside its interval.
+    """
+    if not isinstance(output, TransportSimulation.Output):
+        raise TypeError("output need to be an instance of TransportSimulation.Output")
+
+    critTransp = kwargs.get('critTransp', CRITICAL_TRANSPORTED_FRACTION)
+
+    obj1 = self.baseObjective(output, **kwargs)
+    obj2 = output.transportedFraction / critTransp
+
     return obj1 + weight2 * obj2
 
 
@@ -67,36 +89,52 @@ def linearStep(x, xc):
     """
     return (x - xc) * np.heaviside(x - xc, 0)
 
-def stepObjective(output, weight=100):
+def stepObjective(output, **kwargs):
     """
     Returns the base objective function for when optimizing disruption
     simulations using DREAM, this using step functions.
 
-    :param output:  DREAMSimulation output object.
-    :param weight:  Penalty for when tCQ is outside its interval.
-    """
-    # if not issubclass(output, DREAMSimulation.Output):
-    #     raise TypeError("output need to be a subclass of DREAMSimulation.Output")
+    :param output:      DREAMSimulation output object.
 
-    obj1 = linearStep(output.maxRECurrent, CRITICAL_RE_CURRENT)
-    obj1 = linearStep(output.finalOhmicCurrent, CRITICAL_OHMIC_CURRENT)
+    Optional keyword arguments:
+    :param critRE:      Critical RE current, ~1 penalty when I_re ~ critRE.
+    :param critOhm:     Critical Ohmic current, ~1 penalty when I_ohm ~ critOhm.
+    :param wCQ:         Penalty for when tCQ is outside its interval.
+    """
+    if not isinstance(output, DREAMSimulation.Output):
+        raise TypeError("output need to be a instance of DREAMSimulation.Output")
+
+    critRE  = kwargs.get('critRE',  CRITICAL_RE_CURRENT)
+    critOhm = kwargs.get('critOhm', CRITICAL_OHMIC_CURRENT)
+    wCQ     = kwargs.get('wCQ',     CURRENT_QUENCH_WEIGHT)
+
+    obj1 = linearStep(output.maxRECurrent, critRE)
+    obj2 = linearStep(output.finalOhmicCurrent, critOhm)
     obj3 = sigmoid(-output.currentQuenchTime, -CQ_TIME_MIN, SLOPE_LEFT)
     obj4 = sigmoid(output.currentQuenchTime, CQ_TIME_MAX, SLOPE_RIGHT)
-    return obj1 + weight * (obj2 + obj3)
 
-def stepHeatLossObjective(output, weight1=100, weight2=20):
+    return obj1 + obj2 + wCQ * (obj3 + obj4)
+
+
+def stepHeatLossObjective(output, **kwargs):
     """
     Returns a objective function for when optimizing disruption
     simulations using DREAM that takes into account the conducted heat losses,
     this using step functions.
 
     :param output:      DREAMSimulation output object.
-    :param weight1:     Penalty for when tCQ is outside its interval.
-    :param weight2:     Penalty for when
-    """
-    # if not isinstance(output, TransportSimulation.Output):
-    #     raise TypeError("output need to be an instance of TransportSimulation.Output")
 
-    obj1 = self.stepObjective(output, weight=weight1)
-    obj2 = self.linearStep(output.finalOhmicCurrent, CRITICAL_OHMIC_CURRENT)
-    return obj1 + weight2 * obj2
+    Optional keyword arguments:
+    :param critRE:      Critical RE current, ~1 penalty when I_re ~ critRE.
+    :param critOhm:     Critical Ohmic current, ~1 penalty when I_ohm ~ critOhm.
+    :param wCQ:         Penalty for when tCQ is outside its interval.
+    """
+    if not isinstance(output, TransportSimulation.Output):
+        raise TypeError("output need to be an instance of TransportSimulation.Output")
+
+    critTransp = kwargs.get('critTransp', CRITICAL_TRANSPORTED_FRACTION)
+
+    obj1 = self.stepObjective(output, **kwargs)
+    obj2 = linearStep(output.transportedFraction, critTransp)
+
+    return obj1 + obj2
