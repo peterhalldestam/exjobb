f3aa00b85633db850ac34b640e43cfc3dfa67b9e
Peter <peterhalldestam@outlook.com>
2022-05-11 23:08:21 +0200

    added helper for current density plotter

--- py/utils.py
+++ py/utils.py
@@ -6,6 +6,7 @@ import sys, os
 import numpy as np
 import scipy as scp
 import matplotlib.pyplot as plt
+import colorcet as cc
 import warnings
 
 DREAMPATHS = ('/home/pethalld/DREAM/py', '/home/peterhalldestam/DREAM/py', '/home/hannber/DREAM/py')
@@ -91,6 +92,33 @@ def getDensityProfile(do, n, c):
     profile = .5 * (1 + np.tanh(c * ((do.grid.r/do.grid.a) - .5)))
     return do.grid.r, n * profile * do.grid.integrate(1) / do.grid.integrate(profile)
 
+def visualizeCurrentDensity(t, r, j_ohm, times=[-1], ax=None, show=False):
+    """
+    Plots the current density profile over time.
+
+    :param t:       Simulation time in (ms).
+    :param r:       Minor radius in (m).
+    :param j_ohm:   Ohmic current in (MA/m^-3) of format (t, r).
+    :param times:   Timesteps to plot the current density profile at.
+    :param ax:      matplotlib Axes object.
+    :param show:    Show the figure of the currents.
+    """
+    if ax is None:
+        ax = plt.axes()
+
+    for ti in times:
+        ax.plot(r, j_ohm[ti,:] * 1e-6, label=f't = {t[ti] * 1e3:.1f}')
+
+    ax.legend(title='time (ms)')
+    ax.set_xlabel('minor radius (m)')
+    ax.set_ylabel(r'Ohmic current density (kA/m$^{-3}$)')
+
+    if show:
+        plt.show()
+
+    return ax
+
+
 def visualizeTemperature(r, T, times=[0,-1], ax=None, show=False):
     """
     Plots the temperature profiles of selected times (times=[0,-1] plots the
@@ -124,7 +152,7 @@ def visualizeTemperatureEvolution(t, T, r=[0], ax=None, show=False):
 
     :param t:       Simulation time.
     :param T:       Temperature distribution (NT x NR).
-    :param r:   Radial nodes to plot temperature evolution.
+    :param r:       Radial nodes to plot temperature evolution.
     :param ax:      matplotlib Axes object.
     :param show:    Show the figure of the temperature evolutions.
     """
