236eb60f95eb3f766236730a5a3d5c6a6d174790
Peter Halldestam <peterhalldestam@outlook.com>
2022-05-16 18:15:47 +0200

    1 hyperparameter per input

--- py/opt/bayes/opt3.py
+++ py/opt/bayes/opt3.py
@@ -2,8 +2,9 @@
 import sys, os, glob
 import numpy as np
 
-from bayes_opt import BayesianOptimization
+from sklearn.gaussian_process.kernels import Matern
 
+from bayes_opt import BayesianOptimization
 from bayes_opt.logger import JSONLogger
 from bayes_opt.event import Events
 
@@ -14,7 +15,7 @@ from sim.DREAM.DREAMSimulation import MaximumIterationsException
 
 from opt.objective import baseObjective, heatLossObjective
 
-LOG_PATH = 'data/log_4D_dBB50e-4.json'
+LOG_PATH = 'data/new_log_4D_dBB50e-4.json'
 LARGE_NUMBER = 500
 
 
@@ -37,7 +38,7 @@ def blackBoxFunction(log_nD, log_nNe, cD2, cNe):
         # Penalize bad simulation runs (to avoid these regions)
         for file in glob.glob('outputs/*'):
             os.remove(file)
-            
+
         return -LARGE_NUMBER
 
 
@@ -48,25 +49,33 @@ def main():
 
     # Bounded region of parameter space
     bounds = {'log_nD': (17, 22), 'log_nNe': (15, 21), 'cD2': (-12, 12), 'cNe': (-12, 12)}    # previously (19, 22.2), (15, 19)
-    
+
     nD_ = np.linspace(17, 22, 5)
     nD_ = np.tile(nD_, 5)
-        
+
     nNe_ = np.linspace(18, 20.5, 5)
     nNe_ = np.repeat(nNe_, 5)
-    
+
     #probe = [{'log_nD': nD, 'log_nNe': nNe, 'cD2': 0., 'cNe': 0.} for nD, nNe in zip(nD_, nNe_)]
 
-    optimizer = BayesianOptimization(f=blackBoxFunction, pbounds=bounds, verbose=2, random_state=4)
+    optimizer = BayesianOptimization(
+        f=blackBoxFunction,
+        pbounds=bounds,
+        verbose=2,
+        random_state=420
+    )
 
+    # 1 hyperparameter per input
+    optimizer.set_gp_params(kernel=Matern(length_scale=[1., 1., 1., 1.], nu=2.5))
+
+    # set json logger
     logger = JSONLogger(path=LOG_PATH)
     optimizer.subscribe(Events.OPTIMIZATION_STEP, logger)
 
-    #print('lesgo')
     for i, (nD, nNe) in enumerate(zip(nD_, nNe_)):
         print(f'Probing point {i+1}/{len(nD_)}')
         optimizer.probe(params={'log_nD': nD, 'log_nNe': nNe, 'cD2': 0., 'cNe': 0.}, lazy=False)
-        
+
     print('Probing complete, commencing sampling using acquisition function')
     optimizer.maximize(init_points=0, n_iter=500, acq='ei') # previously n_iter=300
 
